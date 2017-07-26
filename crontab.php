161b5561da9f891c837c2278f40306626647ffea
EC2 Default User <ec2-user@ip-172-31-6-172.ap-southeast-1.compute.internal>
2017-07-26 10:21:02 +0900

    사용자가 test api를 삭제했을 때 crontab 에서 api를 삭제한다

--- crontab.php
+++ crontab.php
@@ -1,11 +1,7 @@
 <?php
 	function insertCommand($crontab_list, $command)
 	{
-		if ($crontab_list == ""){
-			exec('echo "'. $command . '" |crontab -');
-		}
-		else
-			exec('crontab -l | { cat; echo "'. $command . '"; } | crontab -');
+		exec('crontab -l | { cat; echo "'. $command . '"; } | crontab -');
 	}
 	function deleteCommand($command)
 	{
