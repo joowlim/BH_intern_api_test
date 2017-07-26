ac0615d1add39a9e1f1d5ace1a35ffee1fb905a0
rlarlvy153 <rlarlvy153@naver.com>
2017-07-26 13:29:16 +0900

    절대경로 상대경로로 수정

--- modify.php
+++ modify.php
@@ -62,7 +62,7 @@
 				}
 				else{
 					alert("성공");
-					window.location = '/./';
+					window.location = './';
 				}
 				
 			}
@@ -200,7 +200,7 @@
 				}
 				else{
 					alert("성공");
-					window.location = '/./';
+					window.location = './';
 				}
 			}
 		});
